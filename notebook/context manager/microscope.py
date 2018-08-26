a44aee6109a13e038cc5760b17b46531f84d113c
Pietro Berkes <pietro.berkes@googlemail.com>
2018-08-26 16:18:13 +0200

    Material for context managers

---
+++ notebook/context manager/microscope.py
@@ -0,0 +1,92 @@
+import numpy as np
+
+
+class MicroscopeState:
+    def __init__(self):
+        self.under_void = False
+        self.probe_present = False
+        self.connected = False
+
+        
+def connect_to_microscope():
+    microscope_state = MicroscopeState()
+    microscope_state.connected = True
+    print('Connected to microscope')
+    return microscope_state
+
+
+def release_microscope(microscope_state):
+    if not microscope_state.connected:
+        raise RuntimeError('Microscope not connected')
+    
+    microscope_state.connected = False
+    print('Microscope released')
+    
+
+def make_void(microscope_state):
+    if not microscope_state.connected:
+        raise RuntimeError('Microscope not connected')
+
+    microscope_state.under_void = True
+    print('Microscope under void')
+
+
+def release_void(microscope_state):
+    if not microscope_state.connected:
+        raise RuntimeError('Microscope not connected')
+    if not microscope_state.under_void:
+        raise RuntimeError('Microscope not under void')
+    
+    microscope_state.under_void = False
+    print('Void released')
+    
+
+def insert_sample(microscope_state):
+    if not microscope_state.connected:
+        raise RuntimeError('Microscope not connected')
+    if not microscope_state.under_void:
+        raise RuntimeError('Microscope not under void')
+    
+    microscope_state.probe_present = True
+    print('Probe inserted')
+    
+
+def remove_sample(microscope_state):
+    if not microscope_state.connected:
+        raise RuntimeError('Microscope not connected')
+    if not microscope_state.under_void:
+        raise RuntimeError('Microscope not under void')
+    if not microscope_state.probe_present:
+        raise RuntimeError('Probe not present')
+      
+    microscope_state.probe_present = False
+    print('Probe removed')
+
+    
+def calibrate(microscope_state):
+    if not microscope_state.connected:
+        raise RuntimeError('Microscope not connected')
+    if not microscope_state.under_void:
+        raise RuntimeError('Microscope not under void')
+
+    print('Collecting calibration image...')
+    calibration_image = np.zeros((256, 256))
+    print('... done.')
+    
+    return calibration_image
+
+
+def scan_sample(microscope_state):
+    if not microscope_state.connected:
+        raise RuntimeError('Microscope not connected')
+    if not microscope_state.under_void:
+        raise RuntimeError('Microscope not under void')
+    if not microscope_state.probe_present:
+        raise RuntimeError('Probe not present')
+    
+    print('Scanning probe...')
+    sample_image = np.ones((256, 256))
+    print('... done.')
+    
+    return sample_image
+ 
